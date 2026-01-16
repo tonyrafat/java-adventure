84c8973edf73e535ef620846a7fe9c3200b43b3c
tonyrafat <tonyrafat6@gmail.com>
2026-01-15 21:50:34 -0500

    Update Park.java

--- Park.java
+++ Park.java
@@ -1,16 +1,11 @@
 public class Park extends Location {
-
-    public Park() {
-        super("Park", "A peaceful park.");
-    }
-
     public Park(String name, String description) {
         super(name, description);
     }
 
     @Override
     public String onEnter(Player player) {
-        return "A breeze passes through the park. It's calming.";
+        return "The park feels peaceful.";
     }
 }
 
