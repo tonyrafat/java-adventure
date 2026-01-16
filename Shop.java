14c242d03bc726981a3502a9e134a1b013fde5f3
tonyrafat <tonyrafat6@gmail.com>
2026-01-15 21:51:05 -0500

    Update Shop.java

--- Shop.java
+++ Shop.java
@@ -1,15 +1,11 @@
 public class Shop extends Building {
-
-    public Shop() {
-        super("Shop", "A small shop.");
-    }
-
     public Shop(String name, String description) {
         super(name, description);
     }
 
     @Override
     public String onEnter(Player player) {
-        return "You enter the shop. A clerk nods at you.";
+        if (isLocked()) return "The shop is closed.";
+        return "You enter the shop.";
     }
 }
